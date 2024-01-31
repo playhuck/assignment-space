b881dd8a617d984dab5e0198ed3f8d52223183f1
playhuck <wngur89@gmail.com>
2024-01-31 21:49:28 +0900

    ci: 절대경로 설정을 위한 module-alias 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth.module';
+import { ChatModule } from './modules/chat.module';
+import { PostModule } from './modules/post.module';
+import { SpaceModule } from './modules/space.module';
+import { UserModule } from './modules/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ChatModule,
+    PostModule,
+    SpaceModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
