b9d3a4492ae3d5f9d4011f773ac62785cb42a892
playhuck <wngur89@gmail.com>
2024-02-01 00:21:31 +0900

    title: bcrypt 생성 및 설치
    ci:
    - bcrypt 설치
    - @types/bcrypt 설치
    
    create:
    - bcrypt provider 생성

---
+++ src/providers/bcrypt.provider.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+import bcrypt from "bcrypt";
+
+@Injectable()
+export class BcryptProvider {
+
+    private readonly SALT: number;
+
+    constructor(private readonly config: ConfigService) {
+        this.SALT = this.config.get<number>('SALT')!;
+
+    }
+
+    public async hashPassword(inputPassword: string): Promise<string> {
+        try {
+
+            const salt = await bcrypt.genSalt(+this.SALT);
+
+            return await bcrypt.hash(inputPassword, +salt);
+        } catch (err) {
+
+            throw err;
+        }
+    }
+
+    public async comparedPassword(inputPassword: string, existPassword: string): Promise<boolean> {
+        try {
+
+            return await bcrypt.compare(inputPassword, existPassword);
+        } catch (err) {
+            throw err;
+        }
+    };
+
+    public async matchedPassword(password: string, passwordCheck: string): Promise<boolean> {
+
+        return password !== passwordCheck ? false : true
+        
+    };
+
+    public async matchedPwdRegEx(
+        staffPassword: string,
+        staffPasswordCheck: string
+    ){
+
+        const passwordRegex = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+
+        const matchedRegexPwd = passwordRegex.test(staffPassword);
+        const matchedRegexPwdCheck = passwordRegex.test(staffPasswordCheck);
+
+        return {
+            matchedRegexPwd,
+            matchedRegexPwdCheck
+        };
+    };
+
+}
