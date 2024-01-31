43f0c7ef2e4d6ac317b3e4fafaf8aebf49d54a5b
playhuck <wngur89@gmail.com>
2024-01-31 22:36:06 +0900

    create: User Entity 생성

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,54 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    Unique,
+    Index
+} from 'typeorm';
+
+@Entity('user')
+@Unique(['email'])
+@Index('index_email')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    userId!: number;
+
+    @Column({
+        name: 'email',
+        type: 'varchar',
+        length: 128
+    })
+    userEmail!: string;
+
+    @Column({
+        name: 'password', type: 'varchar', length: 50
+    })
+    password!: string;
+
+    @Column({
+        name: 'last_name', type: 'varchar', length: 50
+    })
+    lastName!: string;
+
+    @Column({
+        name: 'first_name', type: 'varchar', length: 50
+    })
+    firstName!: string;
+
+    @Column({
+        name: 'profile_image',
+        type: 'varchar',
+        length: 128,
+        default: 'default_image.jpg'
+    })
+    profileImage!: string;
+
+    @Column({
+        name: 'refresh_token',
+        type: 'varchar',
+        length: 255,
+        nullable: true
+    })
+    refreshToken?: string | null;
+}
